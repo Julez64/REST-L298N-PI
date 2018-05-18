584e551f69ea8c5a8dc0ee47efe0618b06f7e4fe
entrybotjs <entrybotjs@gmail.com>
2018-05-18 17:57:10 -0400

    Changed pins after rebuild

--- api/server.js
+++ api/server.js
@@ -3,11 +3,11 @@ let keypress = require('keypress')
 let process = require('process')
 let sleep = require('thread-sleep')
 
-let gp24 = gpio.export(9, 'out')
-let gp26 = gpio.export(10, 'out')
+let gp24 = gpio.export(2, 'out')
+let gp26 = gpio.export(3, 'out')
 
-let gp19 = gpio.export(8, 'out')
-let gp21 = gpio.export(7, 'out')
+let gp19 = gpio.export(23, 'out')
+let gp21 = gpio.export(24, 'out')
 
 let state = {
 	forward: false,
